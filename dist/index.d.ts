bec9bf1a7f547026ba4db9d9342f4f96d6fd645e
Alex Sazonov <karl93rus@gmail.com>
2020-12-08 14:17:10 +0300

    bug fixes & paste event

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -3,17 +3,16 @@ export interface IOptions {
     hiddenInputId: string;
     fillRecievedValue: boolean;
     filledClass?: string;
-    debug?: boolean;
+    numbersOnly?: boolean;
 }
 export declare class SinChar {
     private digits;
     private resultingPassInput;
     private filledPass;
     private recievedPass;
-    private isFilled;
     private fillRecieved;
-    private debugMode?;
     private filledClass?;
+    private numbersOnly?;
     constructor(options: IOptions);
     processCodeInput(cb?: Function): void;
 }
--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ export interface IOptions {
   hiddenInputId: string;
   fillRecievedValue: boolean;
   filledClass?: string;
-  debug?: boolean
+  numbersOnly?: boolean;
 }
 
 export class SinChar {
@@ -11,25 +11,24 @@ export class SinChar {
   private resultingPassInput: HTMLInputElement; // hidden input to assemble the final string
   private filledPass: boolean; // is pass value emty?
   private recievedPass: string[]; // empty array to store splitted pass value
-  private isFilled: boolean;
   private fillRecieved: boolean;
-  private debugMode?: boolean;
   private filledClass?: string;
+  private numbersOnly?: boolean;
 
   // constructor(selector: string, hiddenInputId: string, debug?: boolean) {
   constructor(options: IOptions) {
     this.digits = Array.from(document.querySelectorAll(options.selector)) as HTMLInputElement[];
     this.resultingPassInput = document.getElementById(options.hiddenInputId) as HTMLInputElement;
     this.filledPass = this.resultingPassInput.value ? true : false;
-    this.fillRecieved = false;
+    this.fillRecieved = options.fillRecievedValue;
     this.recievedPass = [];
-    this.isFilled = false;
 
-    if(options.debug) {
-      this.debugMode = options.debug;
+    if(!options.numbersOnly) {
+      this.numbersOnly = false;
     } else {
-      this.debugMode = false;
+      this.numbersOnly = options.numbersOnly;
     }
+
     if(options.filledClass) {
       this.filledClass = options.filledClass;
     }
@@ -48,99 +47,83 @@ export class SinChar {
       if(this.filledPass && this.fillRecieved){
         // if pass value is not empty
         digit.value = this.recievedPass[index]; // fill every digit with a corresponding recievedPass array element
+        if(this.filledClass) {
+          digit.classList.add(this.filledClass);
+        }
       }
 
+      if(this.filledClass) {
+        digit.addEventListener('focus', () => {
+          digit.classList.add(this.filledClass!);
+        });
+        digit.addEventListener('blur', () => {
+          if(digit.value.length === 0) {
+            digit.classList.remove(this.filledClass!);
+          }
+        });
+      }
+
+      digit.addEventListener('paste', (e: ClipboardEvent) => {
+        const data = e.clipboardData?.getData('text').split('');
+        e.preventDefault();
+
+        if(data && data?.length > 0) {
+          this.digits[index].value = data[index]; // fill every digit with a corresponding pastedata array element
+          this.digits.forEach((d, i) => {
+            this.digits[i].value = data[i];
+            this.resultingPassInput.value += this.digits[i].value;
+            if(this.filledClass) {
+              this.digits[i].classList.add(this.filledClass);
+            }
+          });
+        }
+        if((this.resultingPassInput.value.length === this.digits.length) && cb) {
+          cb();
+        }
+
+      });
+
       digit.addEventListener('keydown', (e: KeyboardEvent) => {
-        if(e.key !== 'Backspace' && index >= 0 && index < this.digits.length -1) {
-          digit.value = e.key;
-          if(this.filledClass && digit.value !== '') {
-            digit.classList.add(this.filledClass);
-          }
-          this.digits[index + 1].focus();
+        if(this.numbersOnly && !/[0-9]|Backspace|Control|v|Tab/.test(e.key)) {
+          console.log(e.key);
           e.preventDefault();
-          e.stopPropagation();
-        } else if(e.key !== 'Backspace' && index === this.digits.length - 1) {
           return;
         }
-
-        if(this.filledClass && e.key === 'Backspace') {
-          digit.classList.remove(this.filledClass);
-          if(this.debugMode) {
-            console.log('keydown', 'digit.value.length === 0:', digit.value.length === 0, 'this.filledClass:', this.filledClass, e.key, digit.classList)
-          }
-        }
-
-        if(digit.value.length === 1 && e.key === 'Backspace' && index > 0) {
-          this.isFilled = false;
-          digit.value = '';
-          return;
-        } else if(digit.value.length === 0 && e.key === 'Backspace' && index > 0) {
-          this.isFilled = false;
-          this.digits[index - 1].value = '';
+        if(digit.value === '' && index > 0 && e.key === 'Backspace') {
           this.digits[index - 1].focus();
-        } else if(e.key === 'Backspace' && index === 0) {
-          this.isFilled = false;
-          return;
+        }
+        if(digit.value !== '' && index > 0 && e.key === 'Backspace') {
+          digit.value = '';
+        }
+        if(digit.value !== '' && index >= 0 && index < this.digits.length - 1 && e.key !== 'Backspace') {
+          this.digits[index + 1].focus();
         }
       });
 
-      digit.addEventListener('keyup', (e: KeyboardEvent) => {
-        if(this.debugMode) {
-          console.log('keyup', this.filledClass, digit.value, index);
-        }
+      digit.addEventListener('input', (e: Event) => {
+        e.preventDefault();
 
-        if(this.filledClass && e.key === 'Backspace') {
-          digit.classList.remove(this.filledClass);
-          if(this.debugMode) {
-            console.log('keydown', 'digit.value.length === 0:', digit.value.length === 0, 'this.filledClass:', this.filledClass, e.key, digit.classList)
-          }
-        }
-
-        if(index === this.digits.length - 1) {
-          if(!/\d/.test(digit.value)) {
-            if(this.debugMode) {
-              console.log('index === this.digits.length - 1', index);
-            }
-            digit.value = '';
-            if(!/\d/.test(this.digits[index - 1].value)) {
-              this.digits[index - 1].value = ''
-              this.digits[index - 1].focus();
-            }
-          }
-        } else if(index === 0) {
-          if(this.debugMode) {
-            console.log('index === 0', index);
-          }
-          if(!/\d/.test(digit.value)) {
-            digit.value = '';
-          }
-        } else if(index > 0 && index < this.digits.length - 1) {
-          if(this.debugMode) {
-            console.log('index > 0 && index < this.digits.length - 1', index);
-          }
-          if(!/\d/.test(this.digits[index - 1].value)) {
-            this.digits[index - 1].value = ''
-            this.digits[index - 1].focus();
-          }
+        if(index >= 0 && index < this.digits.length - 1 && digit.value !== '') {
+          this.digits[index + 1].focus();
         }
 
         this.resultingPassInput.value = '';
         this.digits.forEach(d => {
           this.resultingPassInput.value += d.value;
-          if(this.debugMode) {
-            console.log('result: ', this.resultingPassInput.value);
-          }
         });
-        if(cb && !this.isFilled && this.resultingPassInput.value.length === this.digits.length) {
-          this.isFilled = true;
+
+        if(this.filledClass) {
+          this.digits.forEach(d => {
+            if (d.value !== '') {
+              d.classList.add(this.filledClass!);
+            } else {
+              d.classList.remove(this.filledClass!);
+            }
+          });
+        }
+
+        if((this.resultingPassInput.value.length === this.digits.length) && cb) {
           cb();
-        } else if(!this.isFilled && this.resultingPassInput.value.length === this.digits.length) {
-          if(this.debugMode) {
-            console.log('execute action with value: ', this.resultingPassInput.value);
-          }
-          this.isFilled = true;
-        } else {
-          return;
         }
       });
     });
